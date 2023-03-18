7a22f96366c5946e68005e7e14b10d518782bbc3
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-18 00:11:26 -0400

    finished input collection methods

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// senses for the input changing
+// senses for the input changing on the known letters
 $(document).on("change keyup paste", ".letter", function() {
     // gets the inputs
     let inputs = document.getElementsByClassName("letter");
@@ -12,14 +12,41 @@ $(document).on("change keyup paste", ".letter", function() {
         // initializes color
         let color;
         
-        if (!element.value) {       // checks for an empty letter
-            color = "#ffffff";
-        } else {                    // if not empty make it filled green
-            color = "#26a113";
+        // checks if empty
+        if (!element.value) {       
+            color = "#ffffff";      // colors it white
+        } else {                    
+            color = "#26a113";      // colors it green
         }
 
         // enters color
         element.style.backgroundColor = color;
 
     });
+});
+
+// senses for the input changing on the yellow letters
+$(document).on("change keyup paste", ".letters", function() {
+    
+    // yellow element
+    let yellow = document.querySelector("#known-letters");
+    let not = document.querySelector("#bad-letters");
+    
+    // makes elements uppercase
+    yellow.value = yellow.value.toUpperCase();
+    not.value = not.value.toUpperCase();
+
+    // initializes color
+    let color;
+
+    // checks if its empty
+    if (!yellow.value) {
+        color = "#ffffff";          // color white
+    } else {
+        color = "#d49f00"
+    }
+
+    // sets color
+    yellow.style.backgroundColor = color;
+
 });
